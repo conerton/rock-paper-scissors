3a8023ad7be52fc6e19037a49873eb29ef72b099
Shawn Conerton <conerton52@gmail.com>
2021-06-17 13:23:30 -0500

    deleted

--- test_main.py
+++ test_main.py
@@ -3,11 +3,6 @@ import random
 from main import play
 from main import winner
 
-user = input(
-    "What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n").lower()
-
-computer = random.choice(['r', 'p', 's'])
-
 
 class TestWin(unittest.TestCase):
     def test_winner(self):
